efae68e73a74b0365333f456be4dee5bdaa7b0a5
Jamie Dang <45217971+gothhouse@users.noreply.github.com>
2021-10-28 21:56:26 +0000

    Making the display of dog breeds prettier

--- main.py
+++ main.py
@@ -6,5 +6,25 @@ endpoint = "/api/breeds/list/all"
 url = url_host + endpoint 
 
 response = re.get(url)
+breeds = response.json()["message"]
 
-print(response.json())
+# Get a list of all breeds from the API call
+all_breeds = breeds.keys()
+
+# Loop and print out each key
+for breed in all_breeds:
+  print(breed)
+
+# Get a list of all subbreeds of a breed with at least 3 subbreeds
+# But NOT bulldogs
+# Examples include: poodles, retrievers, terriers, mastiffs, spaniels, setters, & more
+# all_other_subbreeds = breeds["bulldog"]
+# input("\nPress enter to get all bulldog subbreeds")
+# for sub in all_other_subbreeds:
+#   print(sub)
+
+# Display all bulldog subbreeds
+bulldog_subbreeds = breeds["bulldog"]
+input("\nPress enter to get all bulldog subbreeds")
+for sub in bulldog_subbreeds:
+  print(sub)
